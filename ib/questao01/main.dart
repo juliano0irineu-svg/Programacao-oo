ca41454452d32c46039b1379e70bd99fc6d59623
juliano0irineu-svg <juliano0irineu@gmail.com>
2026-03-29 17:07:16 -0300

    Add main.dart to display Pessoa data

---
+++ ib/questao01/main.dart
@@ -0,0 +1,11 @@
+import 'pessoa.dart';
+
+void main() {
+  Pessoa p1 = Pessoa("Ana", 28);
+  Pessoa p2 = Pessoa("Carlos", 35);
+  Pessoa p3 = Pessoa("Beatriz", 17);
+
+  p1.exibirDados();
+  p2.exibirDados();
+  p3.exibirDados();
+}
